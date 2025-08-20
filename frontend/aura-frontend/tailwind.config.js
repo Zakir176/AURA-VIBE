aa9b605e31ada673ecb43f14716f3f2fde20834d
Zakir176 <zakirhusseinmotala76@gmail.com>
2025-08-20 16:49:53 +0200

    updated vue project setup

---
+++ frontend/aura-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}', // <- very important
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
