5d4258a6fe3d308a04c048baec94cd7447b08856
Zakir Motala <zakirhusseinmotala76@gmail.com>
2025-10-17 21:19:23 +0200

    Refactor frontend structure and simplify JoinSession component

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- This is where the routed components will render -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+/* Global styles or component-specific styles */
+</style>
